1a7b9cb866623ecf1f99c1daf0652599c4135eff
S2ShiroeS2 <tuquochung123@gmail.com>
2021-07-29 18:02:46 +0700

    Day 04 complete!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100day';
+  user = {
+    name: 'John Doe',
+    age: 29
+  };
 }
