44ffe329f459de56bf6fdbcc517f1f48cbe166aa
S2ShiroeS2 <tuquochung123@gmail.com>
2021-08-26 12:01:09 +0700

    Review code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
+import { HelloComponent } from './hello/hello.component'; //< -- Here
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent //< -- Here
   ],
   imports: [
     BrowserModule
