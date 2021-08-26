aba106a27924b54aa4c6fa272d69f9855a9b6807
S2ShiroeS2 <tuquochung123@gmail.com>
2021-08-26 15:17:39 +0700

    add code exam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100day';
   isDanger = false;
   isWarning = false;
+  classBox = ['red-border', 'yellow-background']
 }
