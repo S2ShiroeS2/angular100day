d772b593d5f5bd68200d0fc489d050c8778b328e
S2ShiroeS2 <tuquochung123@gmail.com>
2021-08-27 17:34:49 +0700

    ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import { ToggleComponent } from './toggle/toggle.component'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100day';
+  checked = true;
+
+  @ViewChild('toggleComp') toggleComp!: ToggleComponent;
+
+  @ViewChildren(ToggleComponent) toggleList!: QueryList<ToggleComponent>;
+
+  toggleInside() {
+    this.toggleComp.toggle();
+  }
 }
