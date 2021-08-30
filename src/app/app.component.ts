24f215367af4476a397280b58dba41dcaa51eeba
S2ShiroeS2 <tuquochung123@gmail.com>
2021-08-30 17:08:33 +0700

    pipe-in-angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular100day';
+    //MM-DD-YYYY (01-01-1983)
+    date = '2021-8-30';
+    text = "Angular Example";
+    num = 123;
 }
