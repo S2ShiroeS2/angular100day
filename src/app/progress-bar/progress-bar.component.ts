b3808c0417ba2918b8df1c60e351014683e817a6
S2ShiroeS2 <tuquochung123@gmail.com>
2021-08-30 11:17:49 +0700

    Use ngOnchanges validate data

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,31 +1,29 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'app-progress-bar',
     templateUrl: './progress-bar.component.html',
     styleUrls: ['./progress-bar.component.css'],
 })
-export class ProgressBarComponent implements OnInit {
-    private _progress = 50;
-    @Input() set progress(val: number) {
-        //validation input
-        if(typeof val != "number") {
-            const progress = Number(val);
-            if(Number.isNaN(progress)) {
-                this._progress = 0;
-            } else {
-                this._progress = progress;
-            }
-        }
-        this._progress = val;
-    }
-    get progress() {
-        return this._progress;
-    }
+export class ProgressBarComponent implements OnInit, OnChanges {
     @Input() backgroundColor = '#ccc';
     @Input() progressColor = 'tomato';
+    @Input() progress = 50;
 
     constructor() {}
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if('progress' in changes) {
+            if(typeof changes['progress'].currentValue !== 'number') {
+                const progress = Number(changes['progress'].currentValue);
+                if(Number.isNaN(progress)) {
+                    this.progress = 0;
+                } else {
+                    this.progress = progress;
+                }
+            }
+        }
+    }
 
     ngOnInit(): void {}
 }
