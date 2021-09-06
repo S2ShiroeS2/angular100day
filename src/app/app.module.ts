4e46c9918fba91f7f4b5dbf3b1efcfb762712881
S2ShiroeS2 <tuquochung123@gmail.com>
2021-09-06 13:40:34 +0700

    Learn template-driven-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, SignInComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatCheckboxModule,
+		FormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
